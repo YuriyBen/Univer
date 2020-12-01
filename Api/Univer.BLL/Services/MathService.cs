b416ad52b11ca2d0a81a885c53ca7f9150ca411b
OleksiyRiabiy <lehachersemka123@gmail.com>
2020-12-01 20:20:21 +0200

    matrixmultiply

--- Api/Univer.BLL/Services/MathService.cs
+++ Api/Univer.BLL/Services/MathService.cs
@@ -6,10 +6,51 @@ namespace Univer.BLL.Services
 {
     public class MathService : IMathService
     {
-        public int MatrixMultiply(int rows_1, int columns_1, int rows_2, int columns_2)
+        public int[,] MatrixMultiply(int rows_1, int columns_1, int rows_2, int columns_2)
         {
-            return 0;
-        }
+			Random rand = new Random();
+			
+			var matrix1 = new int[rows_1, columns_1];
+			var matrix2 = new int[rows_2, columns_2];
+
+			for (var i = 0; i < rows_1; i++)
+			{
+				for (var j = 0; j < columns_1; j++)
+				{
+					matrix1[i, j] = rand.Next(0, 10); ;
+				}
+			}
+			for (var i = 0; i < rows_2; i++)
+			{
+				for (var j = 0; j < columns_2; j++)
+				{
+					matrix2[i, j] = rand.Next(0, 10); ;
+				}
+			}
+
+			if (columns_1 != rows_2)
+			{
+				throw new Exception(
+					"Multiplication is not possible! " +
+					"The number of columns in the first matrix is ​​not equal to the number of rows in the second matrix.");
+			}
+
+			var matrix3 = new int[rows_1, columns_2];
+
+			for (var i = 0; i < rows_1 ; i++)
+			{
+				for (var j = 0; j < columns_2; j++)
+				{
+					matrix3[i, j] = 0;
+
+					for (var k = 0; k < columns_1; k++)
+					{
+						matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+					}
+				}
+			}
+			return matrix3;
+		}
 
     }
 }
