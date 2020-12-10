b87c1513be7cb5b26ec2fb739b071fede7370321
LybykOleksandr <oleksandr.lybyk@gmail.com>
2020-12-10 19:52:01 +0200

    yeahh

--- ClientApp/src/pages/Home/HomePage.js
+++ ClientApp/src/pages/Home/HomePage.js
@@ -13,45 +13,79 @@ class HomePage extends Component {
 		rangeMin: "",
 		rangeMax: "",
 		enabledGenerate: false,
+		enacleCalculate: false,
 	};
 
 	enabledC = () => {
 		if (
-			(this.state.matrix1C &&
-				this.state.matrix1R &&
-				this.state.matrix2C &&
-				this.state.matrix2R &&
-				this.state.rangeMin &&
-				this.state.rangeMax) !== null
+			this.state.matrix1C !== "" &&
+			this.state.matrix1R !== "" &&
+			this.state.matrix2C !== "" &&
+			this.state.matrix2R !== "" &&
+			this.state.rangeMin !== "" &&
+			this.state.rangeMax !== ""
 		) {
 			this.setState({
 				enabledGenerate: true,
 				matrix2R: this.state.matrix1C,
 			});
+		} else {
+			this.setState({
+				enabledGenerate: false,
+				matrix2R: this.state.matrix1C,
+			});
 		}
-		console.log(this.state);
 	};
 
-	generate = () => {};
+	//TO:range normalnyi bl9
+	generate = () => {
+		var matrix1 = [];
+		for (let i = 0; i < this.state.matrix1R; i++) {
+			matrix1[i] = [];
+			for (let j = 0; j < this.state.matrix1C; j++) {
+				matrix1[i][j] = Math.floor(
+					Math.random() *
+						(this.state.rangeMax - this.state.rangeMin + 1) +
+						this.state.rangeMin
+				);
+			}
+		}
 
-	// claculate = () => {
-	// 	axios
-	// 		.get("/" + )
-	// 		.then((response) => {
-	// 			this.setState({
-	// 				currentAction: response.data,
-	// 			});
-	// 		})
+		var matrix2 = [];
+		for (let i = 0; i < this.state.matrix2R; i++) {
+			matrix2[i] = [];
+			for (let j = 0; j < this.state.matrix2C; j++) {
+				matrix2[i][j] = Math.floor(
+					Math.random() *
+						(this.state.rangeMax - this.state.rangeMin + 1) +
+						this.state.rangeMin
+				);
+			}
+		}
+	};
 
-	// 		.catch((error: any) => {
-	// 			console.log(error);
-	// 		});
-	// };
+	claculate = () => {
+		const matrix = {
+			matrix1: this.state.matrix1,
+			matrix2: this.state.matrix2,
+		};
+		axios
+			.post("/", matrix)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
 	render() {
 		return (
 			<div className={styles.HomePage}>
-				<div>WELCOME, {this.props.userName}, WANNA MULYIPLY SOME MATRIXES?</div>
+				<div>
+					WELCOME, {this.props.userName}, WANNA MULYIPLY SOME
+					MATRIXES?
+				</div>
 				<button onClick={this.props.logout}>LOG OUT</button>
 				<div className={styles.Range}>
 					<div>RANGE OF MATRIX ELEMENTS VALUES</div>
@@ -59,23 +93,34 @@ class HomePage extends Component {
 						placeholder="from"
 						type="number"
 						value={this.state.rangeMin}
-						onChange={event => {
-							this.setState({ rangeMin: event.target.value }, this.enabledC);
+						onChange={(event) => {
+							this.setState(
+								{ rangeMin: event.target.value },
+								this.enabledC
+							);
 						}}
 					/>
 					<input
 						placeholder="to"
 						type="number"
 						value={this.state.rangeMax}
-						onChange={event => {
-							this.setState({ rangeMax: event.target.value }, this.enabledC);
+						onChange={(event) => {
+							this.setState(
+								{ rangeMax: event.target.value },
+								this.enabledC
+							);
 						}}
 					/>
 					<div>
-						<button disabled={!this.state.enabledGenerate} style={{ margin: "15px" }}>
+						<button
+							disabled={!this.state.enabledGenerate}
+							style={{ margin: "15px" }}
+							onClick={this.generate}>
 							GENERATE
 						</button>
-						<button disabled={!this.state.enabledCalculate} style={{ margin: "15px" }}>
+						<button
+							disabled={!this.state.enabledCalculate}
+							style={{ margin: "15px" }}>
 							MULTIPLY
 						</button>
 					</div>
@@ -87,7 +132,7 @@ class HomePage extends Component {
 							placeholder="column"
 							type="number"
 							value={this.state.matrix1C}
-							onChange={event => {
+							onChange={(event) => {
 								this.setState(
 									{
 										matrix1C: event.target.value,
@@ -101,8 +146,11 @@ class HomePage extends Component {
 							placeholder="row"
 							type="number"
 							value={this.state.matrix1R}
-							onChange={event => {
-								this.setState({ matrix1R: event.target.value }, this.enabledC);
+							onChange={(event) => {
+								this.setState(
+									{ matrix1R: event.target.value },
+									this.enabledC
+								);
 							}}
 						/>
 					</div>
@@ -117,11 +165,19 @@ class HomePage extends Component {
 							placeholder="column"
 							type="number"
 							value={this.state.matrix2C}
-							onChange={event => {
-								this.setState({ matrix2C: event.target.value }, this.enabledC);
+							onChange={(event) => {
+								this.setState(
+									{ matrix2C: event.target.value },
+									this.enabledC
+								);
 							}}
 						/>
-						<input placeholder="row" type="number" value={this.state.matrix1C} onChange={() => {}} />
+						<input
+							placeholder="row"
+							type="number"
+							value={this.state.matrix1C}
+							onChange={() => {}}
+						/>
 					</div>
 					<div className={styles.Matrix}>6</div>
 				</div>
@@ -130,14 +186,14 @@ class HomePage extends Component {
 	}
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
 	return {
 		userName: state.userName,
 		isAuthenticated: state.isAuthenticated,
 	};
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
 	return {
 		logout: () => {
 			dispatch({ type: actionTypes.LOG_OUT });
