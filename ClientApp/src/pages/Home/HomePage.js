211c5cd9c6a23a882f0179c4d350fa553cb674a0
SerdiukNaz2018 <serdiuk.nazar2018@gmail.com>
2020-12-10 19:02:25 +0200

    Fix Home Page Layout.

--- ClientApp/src/pages/Home/HomePage.js
+++ ClientApp/src/pages/Home/HomePage.js
@@ -6,12 +6,12 @@ import axios from "axios";
 
 class HomePage extends Component {
 	state = {
-		matrix1R: null,
-		matrix1C: null,
-		matrix2C: null,
-		matrix2R: null,
-		rangeMin: null,
-		rangeMax: null,
+		matrix1R: "",
+		matrix1C: "",
+		matrix2C: "",
+		matrix2R: "",
+		rangeMin: "",
+		rangeMax: "",
 		enabledGenerate: false,
 	};
 
@@ -51,19 +51,43 @@ class HomePage extends Component {
 	render() {
 		return (
 			<div className={styles.HomePage}>
-				<div>
-					<div
-						style={{
-							display: "inline",
-							float: "left",
-							margin: "20px",
-						}}>
-						<div>first matrix</div>
+				<div>WELCOME, {this.props.userName}, WANNA MULYIPLY SOME MATRIXES?</div>
+				<button onClick={this.props.logout}>LOG OUT</button>
+				<div className={styles.Range}>
+					<div>RANGE OF MATRIX ELEMENTS VALUES</div>
+					<input
+						placeholder="from"
+						type="number"
+						value={this.state.rangeMin}
+						onChange={event => {
+							this.setState({ rangeMin: event.target.value }, this.enabledC);
+						}}
+					/>
+					<input
+						placeholder="to"
+						type="number"
+						value={this.state.rangeMax}
+						onChange={event => {
+							this.setState({ rangeMax: event.target.value }, this.enabledC);
+						}}
+					/>
+					<div>
+						<button disabled={!this.state.enabledGenerate} style={{ margin: "15px" }}>
+							GENERATE
+						</button>
+						<button disabled={!this.state.enabledCalculate} style={{ margin: "15px" }}>
+							MULTIPLY
+						</button>
+					</div>
+				</div>
+				<div className={styles.MatrixHolder}>
+					<div className={styles.InputHolder}>
+						<div>FIRST MATRIX</div>
 						<input
 							placeholder="column"
 							type="number"
 							value={this.state.matrix1C}
-							onChange={(event) => {
+							onChange={event => {
 								this.setState(
 									{
 										matrix1C: event.target.value,
@@ -77,102 +101,43 @@ class HomePage extends Component {
 							placeholder="row"
 							type="number"
 							value={this.state.matrix1R}
-							onChange={(event) => {
-								this.setState(
-									{ matrix1R: event.target.value },
-									this.enabledC
-								);
+							onChange={event => {
+								this.setState({ matrix1R: event.target.value }, this.enabledC);
 							}}
 						/>
 					</div>
-					<div
-						style={{
-							display: "inline",
-							float: "right",
-							margin: "20px",
-						}}>
-						<div>second matrix</div>
+					<div className={styles.Matrix}>
+						686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686
+					</div>
+				</div>
+				<div className={styles.MatrixHolder}>
+					<div className={styles.InputHolder}>
+						<div>SECOND MATRIX</div>
 						<input
 							placeholder="column"
 							type="number"
 							value={this.state.matrix2C}
-							onChange={(event) => {
-								this.setState(
-									{ matrix2C: event.target.value },
-									this.enabledC
-								);
+							onChange={event => {
+								this.setState({ matrix2C: event.target.value }, this.enabledC);
 							}}
 						/>
-						<input
-							placeholder="row"
-							type="number"
-							value={this.state.matrix1C}
-						/>
+						<input placeholder="row" type="number" value={this.state.matrix1C} onChange={() => {}} />
 					</div>
-					<div style={{ height: "100px" }}></div>
-					<div className={styles.Range}>
-						<div>input range of random digits</div>
-						<input
-							placeholder="from"
-							type="number"
-							value={this.state.rangeMin}
-							onChange={(event) => {
-								this.setState(
-									{ rangeMin: event.target.value },
-									this.enabledC
-								);
-							}}
-						/>
-						<input
-							placeholder="to"
-							type="number"
-							value={this.state.rangeMax}
-							onChange={(event) => {
-								this.setState(
-									{ rangeMax: event.target.value },
-									this.enabledC
-								);
-							}}
-						/>
-					</div>
-
-					<button
-						disabled={!this.state.enabledGenerate}
-						style={{ margin: "15px" }}>
-						generate
-					</button>
-					<button
-						disabled={!this.state.enabledCalculate}
-						style={{ margin: "15px" }}>
-						calculate
-					</button>
-				</div>
-
-				<div>
-					<div
-						className={styles.Matrix}
-						style={{ float: "left", margin: "15px" }}>
-						686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686686868686868688686
-					</div>
-					<div
-						className={styles.Matrix}
-						style={{ float: "right", margin: "15px" }}>
-						6
-					</div>
+					<div className={styles.Matrix}>6</div>
 				</div>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
 	return {
 		userName: state.userName,
 		isAuthenticated: state.isAuthenticated,
 	};
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
 	return {
 		logout: () => {
 			dispatch({ type: actionTypes.LOG_OUT });
