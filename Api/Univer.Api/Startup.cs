cf47f1cadf7d950f5bec92608af0413cf3f4b767
Yuriy Ben <yur.ben.ten@gmail.com>
2020-12-12 22:53:33 +0200

    Simple docker

--- Api/Univer.Api/Startup.cs
+++ Api/Univer.Api/Startup.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +35,13 @@ namespace Univer.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // load balancer  ->  https://codeburst.io/load-balancing-an-asp-net-core-web-app-using-nginx-and-docker-66753eb08204
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
 
             services.AddCors(options => options.AddPolicy("UniverCorsPolicy",
                              builer => builer.AllowAnyOrigin()
@@ -94,9 +107,40 @@ namespace Univer.Api
             app.UseAuthentication();
             app.UseAuthorization();
 
+            int[] ports = new int[] { 44326, 5000 };
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/plain";
+
+                    // Host info
+                    var name = Dns.GetHostName(); // get container id
+                    var ip = Dns.GetHostEntry(name).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+                    await context.Response.WriteAsync($"Host Name: {Environment.MachineName}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"Port: {ports.OrderBy(n => Guid.NewGuid()).Select(x => x).Take(1).SingleOrDefault()}{Environment.NewLine}");
+                    await context.Response.WriteAsync(Environment.NewLine);
+
+                    // Request method, scheme, and path
+                    await context.Response.WriteAsync($"Request Method: {context.Request.Method}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"Request Scheme: {context.Request.Scheme}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"Request URL: {context.Request.GetDisplayUrl()}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"Request Path: {context.Request.Path}{Environment.NewLine}");
+
+                    // Headers
+                    await context.Response.WriteAsync($"Request Headers:{Environment.NewLine}");
+                    foreach (var (key, value) in context.Request.Headers)
+                    {
+                        await context.Response.WriteAsync($"\t {key}: {value}{Environment.NewLine}");
+                    }
+                    await context.Response.WriteAsync(Environment.NewLine);
+
+                    // Connection: RemoteIp
+                    await context.Response.WriteAsync($"Request Remote IP: {context.Connection.RemoteIpAddress}");
+                });
+
             });
         }
     }
